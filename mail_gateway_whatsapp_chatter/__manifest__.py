b682561d1fb8cef39b9a8d24ff1d741d9a1d8fb5
rodrigo <rodrigomadu85@gmail.com>
2026-07-04 18:17:53 -0300

    [FIX]Alterado gerenciamento de grupo de chatter para módulo iomr_whatsapp

--- mail_gateway_whatsapp_chatter/__manifest__.py
+++ mail_gateway_whatsapp_chatter/__manifest__.py
@@ -9,7 +9,7 @@
     "data": [
         "security/ir.model.access.csv",
         "security/security.xml",
-        "security/menu_groups.xml",
+
         "views/mail_whatsapp_chatter_views.xml",
         "views/whatsapp_composer_views.xml",
     ],
